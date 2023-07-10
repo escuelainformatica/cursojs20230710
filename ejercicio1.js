e5be62f3af7da3ee1be130e036e5dd1d01f0dc18
jorgecc <jcastro@eftec.cl>
2023-07-10 09:19:47 -0400

    1

---
+++ ejercicio1.js
@@ -0,0 +1,3 @@
+let paises=["chile","argentina","peru","bolivia"];
+console.log(paises);
+document.write(paises);
---
+++ ejercicio2.js
@@ -0,0 +1,27 @@
+// codigo1
+let auto={
+    color:"rojo",
+    marca:"toyota",
+    modelo:"yaris"
+}
+console.log(auto);
+// { } = objeto
+// [ ] = arreglo
+let autos=[
+    {
+        color:"rojo",
+        marca:"toyota",
+        modelo:"yaris"
+    },
+    {
+        color:"azul",
+        marca:"renault",
+        modelo:"r20"
+    },
+    {
+        color:"gris",
+        marca:"ford",
+        modelo:"f-150"
+    }
+]
+console.log(autos);
