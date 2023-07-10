01d46087d5fb0ea2ce9b4fbea1daa3bf23d44a79
jorgecc <jcastro@eftec.cl>
2023-07-10 10:59:53 -0400

    6

---
+++ ejercicioconstante.js
@@ -0,0 +1,7 @@
+const cliente="hola";
+console.log(cliente);
+//cliente="mundo"; // error:Assignment to constant variable.
+const paises=["chile","argentina","peru"];
+console.log(paises[0]);
+paises[0]="canada"; // los valores dentro de una constante si son reemplazables.
+console.log(paises[0]);
